467819c2c43b4c76b5f89af81dacd3d191c2c944
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-19 11:27:05 +0100

    About page {{ user }}<
    About page {{ user }}<

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-    <h1>About page</h1>
+    <h1>About page {{ user }}</h1>
     </div>
 </template>
+
+<script setup>
+defineProps({
+    user: String
+})
+</script>
 
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,7 @@ Route::get('/', function () {
     return Inertia::render("Home");
 });
 Route::get('/about', function () {
-    return Inertia("About",[]);
+    return Inertia("About",[
+        'user' =>'Devinsto',
+    ]);
 });
