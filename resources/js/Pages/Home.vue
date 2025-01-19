1cd47f531c71efd99553d7a21eb152bca1e67eee
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-19 18:57:39 +0100

    Gestion du défilement
    Gestion du défilement

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,6 +3,8 @@
     <Layout>
         <Head title="Home" />
         <h1 class="text-7xl text-green-500">Hello home</h1>
+
+        <Link href="/" preserve-scroll>Home</Link>
     </Layout>
 </template>
 
