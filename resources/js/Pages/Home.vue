d7c7d8c38ce10980b22169071a3655e843f549b3
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-19 11:22:23 +0100

    page about
    Les pages d'inertie sont simplement des composants JavaScript. Si vous avez déjà écrit un composant Vue, React ou Svelte, vous vous sentirez comme chez vous. Comme vous pouvez le voir dans l'exemple ci-dessous, les pages reçoivent des données des contrôleurs de votre application en tant qu'accessoires.

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,10 @@
 <template>
     <div>
-        <h1 class="text-3xl text-green-500">Hello home</h1>
+        <h1 class="text-7xl text-green-500">Hello home</h1>
     </div>
 </template>
 
-<script>
-export default {
-    name: 'Home',
-};
+<script setup>
+
 </script>
 
-<style>
-/* Styles ici */
-</style>
