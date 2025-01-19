d3de439a8a54df09c821b35fe12e557f97161965
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-19 23:36:57 +0100

    Validation côté serveur
    La gestion des erreurs de validation côté serveur dans Inertia fonctionne un peu différemment de la gestion des erreurs des requêtes XHR/fetch manuelles. Lorsque vous effectuez des requêtes XHR/fetch, vous inspectez généralement la réponse pour obtenir un 422 code d'état et mettez à jour manuellement l'état d'erreur du formulaire.
    
    Cependant, lorsque vous utilisez Inertia, 422votre serveur ne renvoie jamais de réponse. Au lieu de cela, comme nous l'avons vu dans l'exemple ci-dessus, vos routes/contrôleurs renverront généralement une réponse de redirection, un peu comme une soumission de formulaire classique sur une page entière.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function register (Request $request)
     {
-        dd($request);
+
+        //Validation
+        $fields = $request->validate([
+            'name' => ['required', 'max:255'],
+            'email' => ['required','email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed'],
+
+        ]);
+
+        //S'incrire
+        $user = User::create($fields);
+        
+        //Se connecter
+       Auth::login($user);
+
+        //Rediriger
+        return redirect()->route('home');
+        
+        // dd($request);
     }
 }
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -9,19 +9,24 @@
                 <div class="mb-6">
                     <label for="nom">Nom</label>
                     <input id="nom" type="text"  v-model="form.name"/>
-                    <div v-if="form.errors.name">{{ form.errors.name }}</div>
+                    <div class="text-red-500" v-if="form.errors.name">{{ form.errors.name }}</div>
                 </div>
                 <div class="mb-6">
                     <label for="e-mail">E-mail</label>
                     <input id="e-mail" type="text" v-model="form.email"/>
+                    <div class="text-red-500" v-if="form.errors.email">{{ form.errors.email }}</div>
+
                 </div>
                 <div class="mb-6">
                     <label for="pass">Mot de passe</label>
                     <input id="pass" type="password" v-model="form.password"/>
+                    <div class="text-red-500" v-if="form.errors.password">{{ form.errors.password }}</div>
                 </div>
                 <div class="mb-6">
                     <label for="conf-pass">Confirmez le mot de passe</label>
                     <input id="conf-pass" type="password" v-model="form.password_confirmation"/>
+                    <div class="text-red-500" v-if="form.errors.password">{{ form.errors.password_confirmation }}</div>
+                    
                 </div>
 
                 <div>
@@ -40,10 +45,10 @@
 <script setup>
 import Layout from '../../Layouts/Layout.vue';
 import {reactive} from 'vue'
-import { router } from '@inertiajs/vue3'
+
 import { useForm } from '@inertiajs/vue3'
 
-const form = reactive({
+const form = useForm({
     name:null,
     email:null,
     password:null,
@@ -52,7 +57,7 @@ const form = reactive({
 
 const submit = ()=>{
     // console.log(form);
-    router.post('/register',form)    
+    form.post('/register')    
 }
 </script>
 
