acd6fb3763831eb492e97b9b007f32cbf453c415
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-19 11:30:39 +0100

    Route::inertia('/about',   "About", ['user' =>'Devinsto' ]);
    Route::inertia('/about',   "About", ['user' =>'Devinsto' ]);

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,9 @@ use Inertia\Inertia;
 Route::get('/', function () {
     return Inertia::render("Home");
 });
-Route::get('/about', function () {
-    return Inertia("About",[
-        'user' =>'Devinsto',
-    ]);
-});
+// Route::get('/about', function () {
+//     return Inertia("About",[
+//         'user' =>'Devinsto',
+//     ]);
+// });
+Route::inertia('/about',   "About", ['user' =>'Devinsto' ]);
