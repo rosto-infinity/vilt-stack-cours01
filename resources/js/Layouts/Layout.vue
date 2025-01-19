f68d91343aba7c37cb33f5a8b22616c269cc680c
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-19 11:40:16 +0100

    template Layout.vue
    template Layout.vue

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+
+</script>
+
+<template>
+    <div>
+        <header class="bg-green-600 text-white">
+            <nav class="flex max-w-screen-lg items-center justify-between p-4">
+            <div class="space-x-6">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+            </div>
+            </nav>
+        </header>
+        <main class="p-4">
+            <slot />
+        </main>
+    </div>
+</template>
