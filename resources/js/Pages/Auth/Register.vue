d157904da792afa5c36494ffcc61fa7384f65168
ROSTAND LELE <rostoinfocus@gmail.com>
2025-01-19 22:37:56 +0100

    formulaire Créer un nouveau compte

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -2,14 +2,35 @@
     <Layout>
 
         <Head title="Register" />
-        <h1 class="title">Register a new account</h1>
+        <h1 class="title">Créer un nouveau compte</h1>
 
         <div class="w-2/4 mx-auto">
-        <form>
+            <form @submit.prevent="submit">
+                <div class="mb-6">
+                    <label>Nom</label>
+                    <input type="text" />
+                </div>
+                <div class="mb-6">
+                    <label>E-mail</label>
+                    <input type="text" />
+                </div>
+                <div class="mb-6">
+                    <label>Mot de passe</label>
+                    <input type="password" />
+                </div>
+                <div class="mb-6">
+                    <label>Confirmez le mot de passe</label>
+                    <input type="password" />
+                </div>
 
-
-        </form>
-        </div> 
+                <div>
+                    <p class="text-slate-600 mb-2">
+                        Avez-vous déjà un compte ? <a href="#" class="text-link"> Se connecter</a>
+                    </p>
+                    <button class="primary-btn">S'inscrire</button>
+                </div>
+            </form>
+        </div>
 
     </Layout>
 
@@ -17,7 +38,18 @@
 
 <script setup>
 import Layout from '../../Layouts/Layout.vue';
+import {reactive} from 'vue'
+
+const form = reactive({
+    name:null,
+    email:null,
+    password:null,
+    password_confirmation:null,
+})
+
+const submit = ()=>{
+    console.log('submit');    
+}
 </script>
 
 <style></style>
-
